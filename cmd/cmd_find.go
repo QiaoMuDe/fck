476661a5c6f90289703a72938a7f70fceffbafdc
M乔木 <2776617348@qq.com>
2025-05-25 21:20:38 +0800

    feat(find): 增加隐藏文件和目录的查找功能
    - 新增 -ho 标志，用于限制只显示隐藏文件或目录
    - 优化查找逻辑，支持按文件类型、大小、修改时间等条件进行筛选
    - 重构代码，提高可读性和可维护性

--- cmd/cmd_find.go
+++ cmd/cmd_find.go
@@ -14,56 +14,35 @@ import (
 
 // findCmdMain 是 find 子命令的主函数
 func findCmdMain(cl *colorlib.ColorLib) error {
-	// 检查要查找的路径是否为空
-	if *findCmdPath == "" {
-		return fmt.Errorf("查找路径不能为空")
+	// 检查参数
+	if err := checkFindCmdArgs(); err != nil {
+		return err
 	}
 
-	// 检查要查找的路径是否存在
-	if _, err := os.Stat(*findCmdPath); err != nil {
-		return fmt.Errorf("查找路径不存在: %s", *findCmdPath)
+	// 根据标志决定是否启用正则表达式匹配
+	var escapedKeyword string
+	if *findCmdRegex {
+		// 不转义关键字中的特殊字符, 即启用正则表达式匹配
+		escapedKeyword = *findCmdKeyword
+	} else {
+		// 转义关键字中的特殊字符, 即不启用正则表达式匹配
+		escapedKeyword = regexp.QuoteMeta(*findCmdKeyword)
 	}
-
-	// 检查要查找的最大深度是否小于 -1
-	if *findCmdMaxDepth < -1 {
-		return fmt.Errorf("查找最大深度不能小于 -1")
-	}
-
-	// 检查如果指定了文件大小, 格式是否正确(格式为 +5M 或 -5M), 单位必须为 B/K/M/G 同时为大写
-	if *findCmdSize != "" {
-		// 使用正则表达式匹配文件大小条件
-		sizeRegex := regexp.MustCompile(`^([+-])(\d+)([BKMGbkmg])$`) // 正确分组：符号、数字、单位
-		match := sizeRegex.FindStringSubmatch(*findCmdSize)          // 查找匹配项
-		if match == nil {
-			return fmt.Errorf("文件大小格式错误, 格式如+5M(大于5M)或-5M(小于5M), 支持单位B/K/M/G(大写)")
-		}
-		_, err := strconv.Atoi(match[2]) // 转换数字部分(match[2])
-		if err != nil {
-			return fmt.Errorf("文件大小格式错误")
-		}
-	}
-
-	// 检查是否同时指定了文件和目录和软链接
-	if *findCmdFile && *findCmdDir && *findCmdSymlink {
-		return fmt.Errorf("不能同时指定 -f、-d 和 -l 标志")
-	}
-
-	// 转义关键字中的特殊字符
-	escapedKeyword := regexp.QuoteMeta(*findCmdKeyword)
 
 	// 根据用户选择是否区分大小写
 	var keywordRegex *regexp.Regexp
 	var regexpErr error
+	// 默认不区分大小写
 	if *findCmdCase {
+		// 区分大小写的正则表达式
 		keywordRegex, regexpErr = regexp.Compile(escapedKeyword)
-		if regexpErr != nil {
-			return fmt.Errorf("关键字格式错误: %s", regexpErr)
-		}
+
 	} else {
+		// 不区分大小写的正则表达式
 		keywordRegex, regexpErr = regexp.Compile("(?i)" + escapedKeyword)
-		if regexpErr != nil {
-			return fmt.Errorf("关键字格式错误: %s", regexpErr)
-		}
+	}
+	if regexpErr != nil {
+		return fmt.Errorf("关键字格式错误: %s", regexpErr)
 	}
 
 	// 使用 filepath.WalkDir 遍历目录
@@ -85,10 +64,12 @@ func findCmdMain(cl *colorlib.ColorLib) error {
 				// 如果只查找文件，跳过目录
 				return nil
 			}
+
 			if *findCmdDir && !entry.IsDir() {
 				// 如果只查找目录，跳过文件
 				return nil
 			}
+
 			if *findCmdSymlink {
 				// 如果只查找软链接，检查文件类型
 				fileInfo, linkErr := entry.Info() // 获取文件信息
@@ -99,8 +80,19 @@ func findCmdMain(cl *colorlib.ColorLib) error {
 					return nil
 				}
 			}
+
+			if *findCmdHidden && !isHidden(path) && *findCmdHiddenOnly {
+				// 如果只显示隐藏文件或目录
+				return nil
+			}
+
+			if *findCmdReadOnly && !isReadOnly(path) {
+				// 如果只显示只读文件或目录
+				return nil
+			}
+
 			if *findCmdSize != "" {
-				// 检查文件大小是否符合要求
+				// 如果指定了文件大小, 检查文件大小是否符合要求
 				fileInfo, sizeErr := entry.Info()
 				if sizeErr != nil {
 					return nil
@@ -109,8 +101,9 @@ func findCmdMain(cl *colorlib.ColorLib) error {
 					return nil
 				}
 			}
+
 			if *findCmdModTime != "" {
-				// 检查修改时间是否符合要求
+				// 如果指定了修改时间, 检查文件时间是否符合要求
 				fileInfo, mtimeErr := entry.Info()
 				if mtimeErr != nil {
 					return nil
@@ -129,11 +122,6 @@ func findCmdMain(cl *colorlib.ColorLib) error {
 				}
 
 				// 如果是隐藏文件，跳过单个文件
-				return nil
-			}
-
-			// 检查文件是否为只读文件
-			if *findCmdReadOnly && !isReadOnly(path) {
 				return nil
 			}
 
@@ -168,6 +156,89 @@ func findCmdMain(cl *colorlib.ColorLib) error {
 
 	if walkDirErr != nil {
 		return fmt.Errorf("遍历目录时出错: %s", walkDirErr)
+	}
+
+	return nil
+}
+
+// 用于检查find命令的相关参数是否正确
+func checkFindCmdArgs() error {
+	// 检查要查找的路径是否为空
+	if *findCmdPath == "" {
+		return fmt.Errorf("查找路径不能为空")
+	}
+
+	// 检查要查找的路径是否存在
+	if _, err := os.Stat(*findCmdPath); err != nil {
+		return fmt.Errorf("查找路径不存在: %s", *findCmdPath)
+	}
+
+	// 检查要查找的最大深度是否小于 -1
+	if *findCmdMaxDepth < -1 {
+		return fmt.Errorf("查找最大深度不能小于 -1")
+	}
+
+	// 检查是否同时指定了文件和目录和软链接
+	if *findCmdFile && *findCmdDir && *findCmdSymlink {
+		return fmt.Errorf("不能同时指定 -f、-d 和 -l 标志")
+	}
+
+	// 检查是否同时指定了文件和目录
+	if *findCmdFile && *findCmdDir {
+		return fmt.Errorf("不能同时指定 -f 和 -d 标志")
+	}
+
+	// 检查是否同时指定了文件和软链接
+	if *findCmdFile && *findCmdSymlink {
+		return fmt.Errorf("不能同时指定 -f 和 -l 标志")
+	}
+
+	// 检查是否同时指定了目录和软链接
+	if *findCmdDir && *findCmdSymlink {
+		return fmt.Errorf("不能同时指定 -d 和 -l 标志")
+	}
+
+	// 检查是否同时指定了文件和只读文件
+	if *findCmdFile && *findCmdReadOnly {
+		return fmt.Errorf("不能同时指定 -f 和 -ro 标志")
+	}
+
+	// 检查是否同时指定了目录和只读文件
+	if *findCmdDir && *findCmdReadOnly {
+		return fmt.Errorf("不能同时指定 -d 和 -ro 标志")
+	}
+
+	// 检查如果指定了文件大小, 格式是否正确(格式为 +5M 或 -5M), 单位必须为 B/K/M/G 同时为大写
+	if *findCmdSize != "" {
+		// 使用正则表达式匹配文件大小条件
+		sizeRegex := regexp.MustCompile(`^([+-])(\d+)([BKMGbkmg])$`) // 正确分组：符号、数字、单位
+		match := sizeRegex.FindStringSubmatch(*findCmdSize)          // 查找匹配项
+		if match == nil {
+			return fmt.Errorf("文件大小格式错误, 格式如+5M(大于5M)或-5M(小于5M), 支持单位B/K/M/G(大写)")
+		}
+		_, err := strconv.Atoi(match[2]) // 转换数字部分(match[2])
+		if err != nil {
+			return fmt.Errorf("文件大小格式错误")
+		}
+	}
+
+	// 检查如果指定了修改时间, 格式是否正确(格式为 +5 或 -5), 单位必须为 天
+	if *findCmdModTime != "" {
+		// 使用正则表达式匹配文件时间条件
+		timeRegex := regexp.MustCompile(`^([+-])(\d+)$`) // 正确分组：符号、数字
+		match := timeRegex.FindStringSubmatch(*findCmdModTime)
+		if match == nil {
+			return fmt.Errorf("文件时间格式错误, 格式如+5(5天前)或-5(5天内)")
+		}
+		_, err := strconv.Atoi(match[2]) // 转换数字部分(match[2])
+		if err != nil {
+			return fmt.Errorf("文件时间格式错误")
+		}
+	}
+
+	// 如果只显示隐藏文件或目录, 则必须指定 -hidden 标志
+	if *findCmdHiddenOnly && !*findCmdHidden {
+		return fmt.Errorf("必须指定 -hidden 标志才能使用 -ho 标志")
 	}
 
 	return nil
