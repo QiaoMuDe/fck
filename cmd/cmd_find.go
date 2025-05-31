432f3c476b3c8fb0c18c03326a1635964e49af3e
M乔木 <2776617348@qq.com>
2025-05-31 16:22:33 +0800

    feat(find): 增加路径和文件名匹配功能
    - 新增 -n 和 -p 参数分别用于指定要查找的文件名和路径
    - 增加 -en 和 -ep 参数用于指定要排除的文件名和路径
    - 添加 -and 和 -or 参数用于组合查找条件
    - 重构代码以支持新的匹配逻辑
    - 更新帮助文档和命令行参数定义

--- cmd/cmd_find.go
+++ cmd/cmd_find.go
@@ -31,40 +31,64 @@ func findCmdMain(cl *colorlib.ColorLib, cmd *flag.FlagSet) error {
 	}
 
 	// 根据标志决定是否启用正则表达式匹配
-	var escapedKeyword, exCludedKeyword string
+	var escapedName, escapedPath, exCludedName, exCludedPath string
 	if *findCmdRegex {
 		// 不转义关键字中的特殊字符, 即启用正则表达式匹配
-		escapedKeyword = *findCmdKeyword
+		escapedName = *findCmdName
 
 		// 不转义要排除的关键字中的特殊字符, 即启用正则表达式匹配
-		exCludedKeyword = *findCmdExclude
+		exCludedName = *findCmdExcludeName
+
+		// 如果指定了排除路径, 则转义路径中的特殊字符
+		exCludedPath = *findCmdExcludePath
+
+		// 如果指定了查找路径, 则转义路径中的特殊字符
+		escapedPath = *findCmdPath
 	} else {
 		// 转义关键字中的特殊字符, 即不启用正则表达式匹配
-		escapedKeyword = regexp.QuoteMeta(*findCmdKeyword)
+		escapedName = regexp.QuoteMeta(*findCmdName)
 
 		// 转义要排除的关键字中的特殊字符, 即不启用正则表达式匹配
-		exCludedKeyword = regexp.QuoteMeta(*findCmdExclude)
+		exCludedName = regexp.QuoteMeta(*findCmdExcludeName)
+
+		// 如果指定了排除路径, 则转义路径中的特殊字符
+		exCludedPath = regexp.QuoteMeta(*findCmdExcludePath)
+
+		// 如果指定了查找路径, 则转义路径中的特殊字符
+		escapedPath = regexp.QuoteMeta(*findCmdPath)
 	}
 
 	// 根据用户选择是否区分大小写
-	var keywordRegex, exCludedRegex *regexp.Regexp
-	var regexpErr, exCludedRegexErr error
+	var nameRegex, exNameRegex, pathRegex, exPathRegex *regexp.Regexp
+	var nameRegexErr, exNameRegexErr, pathRegexErr, exPathRegexErr error
 	// 默认不区分大小写
 	if *findCmdCase {
-		// 区分大小写的正则表达式
-		keywordRegex, regexpErr = regexp.Compile(escapedKeyword)
+		// 编译查找文件或目录名的正则表达式
+		nameRegex, nameRegexErr = regexp.Compile(escapedName)
 
 		// 编译排除的关键字的正则表达式
-		exCludedRegex, exCludedRegexErr = regexp.Compile(exCludedKeyword)
+		exNameRegex, exNameRegexErr = regexp.Compile(exCludedName)
+
+		// 编译查找路径的正则表达式
+		pathRegex, pathRegexErr = regexp.Compile(escapedPath)
+
+		// 编译排除路径的正则表达式
+		exPathRegex, exPathRegexErr = regexp.Compile(exCludedPath)
 	} else {
 		// 不区分大小写的正则表达式
-		keywordRegex, regexpErr = regexp.Compile("(?i)" + escapedKeyword)
+		nameRegex, nameRegexErr = regexp.Compile("(?i)" + escapedName)
 
 		// 编译排除的关键字的正则表达式
-		exCludedRegex, exCludedRegexErr = regexp.Compile("(?i)" + exCludedKeyword)
+		exNameRegex, exNameRegexErr = regexp.Compile("(?i)" + exCludedName)
+
+		// 编译查找路径的正则表达式
+		pathRegex, pathRegexErr = regexp.Compile("(?i)" + escapedPath)
+
+		// 编译排除路径的正则表达式
+		exPathRegex, exPathRegexErr = regexp.Compile("(?i)" + exCludedPath)
 	}
-	if regexpErr != nil || exCludedRegexErr != nil {
-		return fmt.Errorf("关键字格式错误: %s", regexpErr)
+	if nameRegexErr != nil || exNameRegexErr != nil || pathRegexErr != nil || exPathRegexErr != nil {
+		return fmt.Errorf("表达式编译错误: %v, %v, %v, %v", nameRegexErr, exNameRegexErr, pathRegexErr, exPathRegexErr)
 	}
 
 	// 使用 filepath.WalkDir 遍历目录
@@ -98,167 +122,54 @@ func findCmdMain(cl *colorlib.ColorLib, cmd *flag.FlagSet) error {
 			}
 		}
 
-		// 检查文件名是否匹配关键字并根据标志决定是否输出
-		if keywordRegex.MatchString(entry.Name()) {
-			// 如果只查找文件，跳过目录
-			if *findCmdFile && entry.IsDir() {
-				return nil
-			}
-
-			// 如果只查找目录，跳过文件
-			if *findCmdDir && !entry.IsDir() {
-				return nil
-			}
-
-			// 如果只查找软链接，检查文件类型
-			if *findCmdSymlink {
-				fileInfo, linkErr := os.Lstat(path)
-				if linkErr != nil {
-					return nil
-				}
-
-				// Windows系统特殊处理
-				if runtime.GOOS == "windows" {
-					// 检查.lnk后缀
-					if strings.HasSuffix(entry.Name(), ".lnk") {
-						// 是快捷方式，继续后续处理
-					} else if fileInfo.Mode()&os.ModeSymlink != 0 {
-						// 是mklink创建的符号链接
-					} else {
-						// 既不是.lnk也不是符号链接, 跳过
-						return nil
-					}
-				} else {
-					// 非Windows系统只检查ModeSymlink标志
-					if fileInfo.Mode()&os.ModeSymlink == 0 {
-						return nil // 不是符号链接, 跳过
-					}
-				}
-			}
-
-			// 如果只显示隐藏文件或目录
-			if *findCmdHidden && !isHidden(path) && *findCmdHiddenOnly {
-				return nil
-			}
-
-			// 如果只显示只读文件或目录
-			if *findCmdReadOnly && !isReadOnly(path) {
-				return nil
-			}
-
-			// 如果指定了文件大小, 检查文件大小是否符合要求
-			if *findCmdSize != "" {
-				fileInfo, sizeErr := entry.Info()
-				if sizeErr != nil {
-					return nil
-				}
-				if !matchFileSize(fileInfo.Size(), *findCmdSize) {
-					return nil
-				}
-			}
-
-			// 如果指定了修改时间, 检查文件时间是否符合要求
-			if *findCmdModTime != "" {
-				fileInfo, mtimeErr := entry.Info()
-				if mtimeErr != nil {
-					return nil
-				}
-				// 检查文件时间是否符合要求
-				if !matchFileTime(fileInfo.ModTime(), *findCmdModTime) {
-					return nil
-				}
-			}
-
-			// 检查路径是否为隐藏文件或目录(默认不显示隐藏文件)
-			if !*findCmdHidden && isHidden(path) {
-				// 如果是隐藏目录，跳过整个目录
-				if entry.IsDir() {
-					return filepath.SkipDir
-				}
-
-				// 如果是隐藏文件，跳过单个文件
-				return nil
-			}
-
-			// 检查路径中是否包含排除的关键字或目录
-			if *findCmdExclude != "" && exCludedRegex.MatchString(path) {
-				return nil
-			}
-
-			// 如果启用了删除标志，删除匹配的文件或目录
-			if *findCmdDelete {
-				if err := deleteMatchedItem(path, entry.IsDir()); err != nil {
+		// 如果指定了-n和-p参数, 并且指定了-or参数, 则只检查文件名或路径是否匹配(默认为或操作)
+		if *findCmdOr && *findCmdName != "" && *findCmdPath != "" {
+			// 执行或操作
+			if nameRegex.MatchString(entry.Name()) || pathRegex.MatchString(path) {
+				if err := filterConditions(entry, path, cl, exNameRegex, exPathRegex); err != nil {
 					return err
 				}
-
-				// 如果是目录, 跳过整个目录
-				if entry.IsDir() {
-					return filepath.SkipDir
-				}
-
-				// 如果是文件, 跳过单个文件
-				return nil
 			}
-
-			// 如果启用了-mv标志，移动匹配的文件或目录
-			if *findCmdMove != "" {
-				if err := moveMatchedItem(path, *findCmdMove, cl); err != nil {
-					return err
-				}
-
-				// 如果是目录, 跳过整个目录
-				if entry.IsDir() {
-					return filepath.SkipDir
-				}
-
-				// 如果是文件, 跳过单个文件
-				return nil
-			}
-
-			// 如果指定了-exec命令, 则执行命令
-			if *findCmdExec != "" {
-				// 替换{}为实际的文件路径，并根据系统类型选择引用方式
-				var cmdStr string
-				if runtime.GOOS == "windows" {
-					cmdStr = strings.Replace(*findCmdExec, "{}", fmt.Sprintf("\"%s\"", path), -1) // Windows使用双引号
-				} else {
-					cmdStr = strings.Replace(*findCmdExec, "{}", fmt.Sprintf("'%s'", path), -1) // Linux使用单引号
-				}
-
-				// 执行命令
-				if err := runCommand(cmdStr, cl); err != nil {
-					return fmt.Errorf("执行-exec命令时发生了错误: %v", err)
-				}
-
-				return nil
-			}
-
-			// 输出匹配的文件或目录路径
-			if *findCmdFullPath {
-				// 获取完整路径
-				fullPath, pathErr := filepath.Abs(path)
-				if pathErr != nil {
-					return fmt.Errorf("获取完整路径时出错: %s", pathErr)
-				}
-				// 输出完整路径
-				if *findCmdColor {
-					if err := printPathColor(fullPath, cl); err != nil {
-						return fmt.Errorf("输出路径时出错: %s", err)
-					}
-				} else {
-					fmt.Println(fullPath)
-				}
-			} else {
-				// 输出相对路径
-				if *findCmdColor {
-					if err := printPathColor(path, cl); err != nil {
-						return fmt.Errorf("输出路径时出错: %s", err)
-					}
-				} else {
-					fmt.Println(path)
-				}
-			}
+			return nil
 		}
+
+		// 如果指定了-n和-p参数, 则同时检查文件名和路径是否匹配(默认为-and操作)
+		if *findCmdAnd && *findCmdName != "" && *findCmdPath != "" {
+			if nameRegex.MatchString(entry.Name()) && pathRegex.MatchString(path) {
+				// 如果同时匹配, 则执行筛选条件
+				if err := filterConditions(entry, path, cl, exNameRegex, exPathRegex); err != nil {
+					return err
+				}
+			}
+
+			return nil
+		}
+
+		// 如果指定了-n参数, 则检查文件名是否匹配
+		if *findCmdName != "" {
+			if nameRegex.MatchString(entry.Name()) {
+				if err := filterConditions(entry, path, cl, exNameRegex, exPathRegex); err != nil {
+					return err
+				}
+			}
+			return nil
+		}
+
+		// 如果指定了-p参数, 则检查路径是否匹配
+		if *findCmdPath != "" {
+			if pathRegex.MatchString(path) {
+				if err := filterConditions(entry, path, cl, exNameRegex, exPathRegex); err != nil {
+					return err
+				}
+			}
+			return nil
+		}
+
+		// 默认情况下
+		if err := filterConditions(entry, path, cl, exNameRegex, exPathRegex); err != nil {
+			return err
+		}
+
 		return nil
 	})
 
@@ -269,6 +180,178 @@ func findCmdMain(cl *colorlib.ColorLib, cmd *flag.FlagSet) error {
 			return fmt.Errorf("路径不存在: %v", walkDirErr)
 		}
 		return fmt.Errorf("遍历目录时出错: %v", walkDirErr)
+	}
+
+	return nil
+}
+
+// 用于在循环中筛选条件的函数
+func filterConditions(entry os.DirEntry, path string, cl *colorlib.ColorLib, exNameRegex, exPathRegex *regexp.Regexp) error {
+	// 如果只查找文件，跳过目录
+	if *findCmdFile && entry.IsDir() {
+		return nil
+	}
+
+	// 如果只查找目录，跳过文件
+	if *findCmdDir && !entry.IsDir() {
+		return nil
+	}
+
+	// 如果只查找软链接，跳过非软链接
+	if *findCmdSymlink {
+		fileInfo, linkErr := os.Lstat(path)
+		if linkErr != nil {
+			return nil
+		}
+
+		// Windows系统特殊处理
+		if runtime.GOOS == "windows" {
+			// 检查.lnk后缀
+			if strings.HasSuffix(entry.Name(), ".lnk") {
+				// 是快捷方式，继续后续处理
+			} else if fileInfo.Mode()&os.ModeSymlink != 0 {
+				// 是mklink创建的符号链接
+			} else {
+				// 既不是.lnk也不是符号链接, 跳过
+				return nil
+			}
+		} else {
+			// 非Windows系统只检查ModeSymlink标志
+			if fileInfo.Mode()&os.ModeSymlink == 0 {
+				return nil // 不是符号链接, 跳过
+			}
+		}
+	}
+
+	// 如果只显示隐藏文件或目录, 跳过非隐藏文件或目录
+	if *findCmdHidden && !isHidden(path) && *findCmdHiddenOnly {
+		return nil
+	}
+
+	// 如果只显示只读文件或目录 跳过非只读文件或目录
+	if *findCmdReadOnly && !isReadOnly(path) {
+		return nil
+	}
+
+	// 如果指定了文件大小, 跳过不符合条件的文件
+	if *findCmdSize != "" {
+		fileInfo, sizeErr := entry.Info()
+		if sizeErr != nil {
+			return nil
+		}
+		if !matchFileSize(fileInfo.Size(), *findCmdSize) {
+			return nil
+		}
+	}
+
+	// 如果指定了修改时间, 跳过不符合条件的文件
+	if *findCmdModTime != "" {
+		fileInfo, mtimeErr := entry.Info()
+		if mtimeErr != nil {
+			return nil
+		}
+		// 检查文件时间是否符合要求
+		if !matchFileTime(fileInfo.ModTime(), *findCmdModTime) {
+			return nil
+		}
+	}
+
+	// 如果没有启用隐藏标志且是隐藏目录或文件, 则跳过
+	if !*findCmdHidden && isHidden(path) {
+		// 如果是隐藏目录，跳过整个目录
+		if entry.IsDir() {
+			return filepath.SkipDir
+		}
+
+		// 如果是隐藏文件，跳过单个文件
+		return nil
+	}
+
+	// 如果指定了排除文件或目录名, 跳过匹配的文件或目录
+	if *findCmdExcludeName != "" && exNameRegex.MatchString(entry.Name()) {
+		return nil
+	}
+
+	// 如果指定了排除路径, 跳过匹配的路径
+	if *findCmdExcludePath != "" && exPathRegex.MatchString(path) {
+		return nil
+	}
+
+	// 如果启用了delete标志, 删除匹配的文件或目录
+	if *findCmdDelete {
+		if err := deleteMatchedItem(path, entry.IsDir()); err != nil {
+			return err
+		}
+
+		// 如果是目录, 跳过整个目录
+		if entry.IsDir() {
+			return filepath.SkipDir
+		}
+
+		// 如果是文件, 跳过单个文件
+		return nil
+	}
+
+	// 如果启用了-mv标志, 将匹配的文件或目录移动到指定位置
+	if *findCmdMove != "" {
+		if err := moveMatchedItem(path, *findCmdMove, cl); err != nil {
+			return err
+		}
+
+		// 如果是目录, 跳过整个目录
+		if entry.IsDir() {
+			return filepath.SkipDir
+		}
+
+		// 如果是文件, 跳过单个文件
+		return nil
+	}
+
+	// 如果启用了-exec标志, 执行指定的命令
+	if *findCmdExec != "" {
+		// 替换{}为实际的文件路径，并根据系统类型选择引用方式
+		var cmdStr string
+		if runtime.GOOS == "windows" {
+			cmdStr = strings.Replace(*findCmdExec, "{}", fmt.Sprintf("\"%s\"", path), -1) // Windows使用双引号
+		} else {
+			cmdStr = strings.Replace(*findCmdExec, "{}", fmt.Sprintf("'%s'", path), -1) // Linux使用单引号
+		}
+
+		// 执行命令
+		if err := runCommand(cmdStr, cl); err != nil {
+			return fmt.Errorf("执行-exec命令时发生了错误: %v", err)
+		}
+
+		return nil
+	}
+
+	// 根据标志, 输出输出完整路径还是匹配到的路径
+	if *findCmdFullPath {
+		// 获取完整路径
+		fullPath, pathErr := filepath.Abs(path)
+		if pathErr != nil {
+			return fmt.Errorf("获取完整路径时出错: %s", pathErr)
+		}
+
+		// 输出完整路径
+		if *findCmdColor {
+			if err := printPathColor(fullPath, cl); err != nil {
+				return fmt.Errorf("输出路径时出错: %s", err)
+			}
+		} else {
+			// 如果没有启用颜色输出, 直接打印路径
+			fmt.Println(fullPath)
+		}
+	} else {
+		// 输出匹配到的路径
+		if *findCmdColor {
+			if err := printPathColor(path, cl); err != nil {
+				return fmt.Errorf("输出路径时出错: %s", err)
+			}
+		} else {
+			// 如果没有启用颜色输出, 直接打印路径
+			fmt.Println(path)
+		}
 	}
 
 	return nil
@@ -327,6 +410,16 @@ func checkFindCmdArgs(findPath string) error {
 		return fmt.Errorf("不能同时指定 -d 和 -ro 标志")
 	}
 
+	// 如果只显示隐藏文件或目录, 则必须指定 -H 标志
+	if *findCmdHiddenOnly && !*findCmdHidden {
+		return fmt.Errorf("必须指定 -H 标志才能使用 -ho 标志")
+	}
+
+	// 检查是否同时指定了-or
+	if *findCmdOr {
+		*findCmdAnd = false // 如果使用-or, 则不能同时使用-and
+	}
+
 	// 检查如果指定了文件大小, 格式是否正确(格式为 +5M 或 -5M), 单位必须为 B/K/M/G 同时为大写
 	if *findCmdSize != "" {
 		// 使用正则表达式匹配文件大小条件
@@ -353,11 +446,6 @@ func checkFindCmdArgs(findPath string) error {
 		if err != nil {
 			return fmt.Errorf("文件时间格式错误")
 		}
-	}
-
-	// 如果只显示隐藏文件或目录, 则必须指定 -H 标志
-	if *findCmdHiddenOnly && !*findCmdHidden {
-		return fmt.Errorf("必须指定 -H 标志才能使用 -ho 标志")
 	}
 
 	// 检查-exec标志是否包含{}
