92ff97b95a0ea935fab1c003f1003af4de51e0b1
M乔木 <2776617348@qq.com>
2025-06-09 17:58:28 +0800

    feat(cmd): 优化颜色配置并支持跨平台
    - 将颜色映射分为 windowsColorMap 和 linuxColorMap，根据不同操作系统使用不同的颜色配置
    - 新增 linuxColorMap，增加了对配置文件、压缩文件等类型的颜色支持
    - 修改 print

--- cmd/cmd_color.go
+++ cmd/cmd_color.go
@@ -4,26 +4,23 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 
 	"gitee.com/MM-Q/colorlib"
 	"gitee.com/MM-Q/fck/globals"
 )
 
-// ColorMap 定义了不同颜色对应的文件后缀名映射
-var ColorMap = map[string]map[string]bool{
+// windowsColorMap 定义了不同颜色对应的文件后缀名映射
+var windowsColorMap = map[string]map[string]bool{
 	"red": {
 		".exe":   true, // Windows可执行文件
-		".bin":   true, // 二进制可执行文件
 		".app":   true, // macOS应用程序
 		".msi":   true, // Windows安装程序
 		".dll":   true, // Windows动态链接库
 		".so":    true, // Linux共享库
 		".run":   true, // Linux可执行文件
 		".dylib": true, // macOS动态库
-		".sh":    true, // Shell脚本
-		".bash":  true, // Bash脚本
-		".zsh":   true, // Zsh脚本
 		".bat":   true, // Windows批处理脚本
 		".ps1":   true, // PowerShell脚本
 		".wasm":  true, // WebAssembly文件
@@ -59,49 +56,44 @@ var ColorMap = map[string]map[string]bool{
 		".swo":           true, // Vim交换文件
 	},
 	"green": {
-		".c":       true, // C源文件
-		".cpp":     true, // C++源文件
-		".h":       true, // C头文件
-		".hpp":     true, // C++头文件
-		".go":      true, // Go源文件
-		".py":      true, // Python脚本
-		".class":   true, // Java字节码文件
-		".js":      true, // JavaScript文件
-		".ts":      true, // TypeScript文件
-		".html":    true, // HTML文件
-		".css":     true, // CSS样式表
-		".java":    true, // Java源文件
-		".pyd":     true, // Python字节码文件
-		".pyc":     true, // Python编译文件
-		".pyo":     true, // Python编译文件
-		".pyw":     true, // Python脚本
-		".rb":      true, // Ruby脚本
-		".rs":      true, // Rust脚本
-		".php":     true, // PHP脚本
-		".swift":   true, // Swift脚本
-		".kotlin":  true, // Kotlin脚本
-		".scala":   true, // Scala脚本
-		".elm":     true, // Elm脚本
-		".lua":     true, // Lua脚本
-		".pl":      true, // Perl脚本
-		".perl":    true, // Perl脚本
-		".r":       true, // R语言脚本
-		".vbs":     true, // VBScript文件
-		".psm1":    true, // PowerShell模块文件
-		".s":       true, // 汇编语言源文件
-		".o":       true, // 编译后的目标文件
-		".a":       true, // 静态库文件
-		".lib":     true, // 静态库文件
-		".sql":     true, // SQL脚本文件
-		".db":      true, // 数据库文件
-		".db3":     true, // SQLite数据库文件
-		".sqlite3": true, // SQLite数据库文件
-		".sqlite":  true, // SQLite数据库文件
-		".dbf":     true, // DBF文件
-		".mdb":     true, // Access数据库文件
-		".accdb":   true, // Access数据库文件
-		".dbx":     true, // DBX文件
-		".pdb":     true, // 调试信息文件
+		".sh":     true, // Shell脚本
+		".bash":   true, // Bash脚本
+		".zsh":    true, // Zsh脚本
+		".c":      true, // C源文件
+		".cpp":    true, // C++源文件
+		".h":      true, // C头文件
+		".hpp":    true, // C++头文件
+		".go":     true, // Go源文件
+		".py":     true, // Python脚本
+		".class":  true, // Java字节码文件
+		".js":     true, // JavaScript文件
+		".ts":     true, // TypeScript文件
+		".html":   true, // HTML文件
+		".css":    true, // CSS样式表
+		".java":   true, // Java源文件
+		".pyd":    true, // Python字节码文件
+		".pyc":    true, // Python编译文件
+		".pyo":    true, // Python编译文件
+		".pyw":    true, // Python脚本
+		".rb":     true, // Ruby脚本
+		".rs":     true, // Rust脚本
+		".php":    true, // PHP脚本
+		".swift":  true, // Swift脚本
+		".kotlin": true, // Kotlin脚本
+		".scala":  true, // Scala脚本
+		".elm":    true, // Elm脚本
+		".lua":    true, // Lua脚本
+		".pl":     true, // Perl脚本
+		".perl":   true, // Perl脚本
+		".r":      true, // R语言脚本
+		".vbs":    true, // VBScript文件
+		".psm1":   true, // PowerShell模块文件
+		".s":      true, // 汇编语言源文件
+		".o":      true, // 编译后的目标文件
+		".a":      true, // 静态库文件
+		".lib":    true, // 静态库文件
+		".sql":    true, // SQL脚本文件
+
 	},
 	"purple": {
 		".zip":     true, // ZIP压缩文件
@@ -128,6 +120,62 @@ var ColorMap = map[string]map[string]bool{
 	},
 }
 
+// linuxColorMap 定义了不同颜色对应的文件后缀名映射
+var linuxColorMap = map[string]map[string]bool{
+	"yellow": {
+		".md":            true, // Markdown文件
+		".json":          true, // JSON文件
+		".jsonl":         true, // JSON Lines文件
+		".xml":           true, // XML文件
+		".yaml":          true, // YAML文件
+		".yml":           true, // YAML配置文件
+		".toml":          true, // TOML配置文件
+		".ini":           true, // INI配置文件
+		".conf":          true, // 配置文件
+		".cfg":           true, // 配置文件
+		".properties":    true, // Java属性文件
+		".env":           true, // 环境变量文件
+		".gitignore":     true, // Git忽略文件
+		".gitattributes": true, // Git属性配置文件
+		".gitmodules":    true, // Git子模块配置文件
+		".gitkeep":       true, // Git保留文件
+		".gitconfig":     true, // Git配置文件
+		".git":           true, // Git版本控制系统文件
+		".svn":           true, // Subversion版本控制系统文件
+		".hg":            true, // Mercurial版本控制系统文件
+		".bzr":           true, // Bazaar版本控制系统文件
+		".lock":          true, // 锁文件
+		".lockfile":      true, // 锁文件
+		".bak":           true, // 备份文件
+		".bakup":         true, // 备份文件
+		".tmp":           true, // 临时文件
+		".swp":           true, // Vim交换文件
+		".swo":           true, // Vim交换文件
+		".mod":           true, // Go模块文件
+		".sum":           true, // Go模块文件
+	},
+	"red": {
+		".zip":     true, // ZIP压缩文件
+		".tar":     true, // TAR压缩文件
+		".gz":      true, // GZIP压缩文件
+		".bz2":     true, // BZIP2压缩文件
+		".xz":      true, // XZ压缩文件
+		".7z":      true, // 7Z压缩文件
+		".rar":     true, // RAR压缩文件
+		".tar.gz":  true, // TAR.GZ压缩文件
+		".tar.bz2": true, // TAR.BZ2压缩文件
+		".tar.xz":  true, // TAR.XZ压缩文件
+		".tgz":     true, // TAR.GZ压缩文件
+		".tbz2":    true, // TAR.BZ2压缩文件
+		".txz":     true, // TAR.XZ压缩文件
+		".jar":     true, // Java Archive文件
+		".war":     true, // Web Archive文件
+		".ear":     true, // Enterprise Archive文件
+		".apk":     true, // Android应用包
+		".ipa":     true, // iOS应用包
+	},
+}
+
 // 定义全局常量的颜色映射
 var PermissionColorMap = map[int]string{
 	1: "green",  // 所有者-读-绿色
@@ -143,10 +191,18 @@ var PermissionColorMap = map[int]string{
 
 // printColoredFile 根据文件后缀名以不同颜色输出文件路径
 func printColoredFile(fs string, cl *colorlib.ColorLib) {
-
 	// 获取文件后缀名
 	fileExt := strings.ToLower(filepath.Ext(fs))
 
+	// 根据操作系统获取map
+	var ColorMap map[string]map[string]bool
+	if runtime.GOOS == "windows" {
+		ColorMap = windowsColorMap
+	} else {
+		ColorMap = linuxColorMap
+	}
+
+	// 根据文件后缀名输出不同颜色的文件路径
 	for color, extensions := range ColorMap {
 		if extensions[fileExt] {
 			switch color {
@@ -303,7 +359,6 @@ func SprintStringColor(p string, s string, cl *colorlib.ColorLib) (string, error
 //
 //	error: 如果获取路径信息失败则返回错误, 否则返回nil
 func printPathColor(path string, cl *colorlib.ColorLib) error {
-
 	// 获取路径信息
 	pathInfo, statErr := os.Lstat(path)
 	if statErr != nil {
@@ -312,17 +367,6 @@ func printPathColor(path string, cl *colorlib.ColorLib) error {
 
 	// 根据路径类型设置颜色
 	switch mode := pathInfo.Mode(); {
-	case mode.IsDir():
-		// 目录 - 使用蓝色输出
-		// 检查是否包含目录分割符
-		if strings.Contains(path, string(os.PathSeparator)) {
-			// 把路径分割成目录和文件名
-			dir, file := filepath.Split(path)
-
-			fmt.Println(cl.Scyan(dir) + cl.Sblue(file))
-		} else {
-			cl.Blue(path) // 如果没有目录分割符，则直接输出文件名
-		}
 	case mode&os.ModeSymlink != 0:
 		// 符号链接 - 使用青色输出
 		// 检查是否包含目录分割符
@@ -333,6 +377,17 @@ func printPathColor(path string, cl *colorlib.ColorLib) error {
 			fmt.Println(cl.Scyan(dir) + cl.Scyan(file))
 		} else {
 			cl.Cyan(path) // 如果没有目录分割符，则直接输出文件名
+		}
+	case mode.IsDir():
+		// 目录 - 使用蓝色输出
+		// 检查是否包含目录分割符
+		if strings.Contains(path, string(os.PathSeparator)) {
+			// 把路径分割成目录和文件名
+			dir, file := filepath.Split(path)
+
+			fmt.Println(cl.Scyan(dir) + cl.Sblue(file))
+		} else {
+			cl.Blue(path) // 如果没有目录分割符，则直接输出文件名
 		}
 	case mode&os.ModeDevice != 0:
 		// 设备文件 - 使用黄色输出
@@ -368,14 +423,14 @@ func printPathColor(path string, cl *colorlib.ColorLib) error {
 			cl.Yellow(path) // 如果没有目录分割符，则直接输出文件名
 		}
 	case mode&os.ModeType == 0 && mode&0111 != 0:
-		// 可执行文件 - 使用红色输出
+		// 可执行文件 - 使用绿色输出
 		// 检查是否包含目录分割符
 		if strings.Contains(path, string(os.PathSeparator)) {
 			// 把路径分割成目录和文件名
 			dir, file := filepath.Split(path)
-			fmt.Println(cl.Scyan(dir) + cl.Sred(file))
+			fmt.Println(cl.Scyan(dir) + cl.Sgreen(file))
 		} else {
-			cl.Red(path) // 如果没有目录分割符，则直接输出文件名
+			cl.Green(path) // 如果没有目录分割符，则直接输出文件名
 		}
 	case mode.IsRegular():
 		printColoredFile(path, cl) // 普通文件 - 用于根据文件名的后缀输出
@@ -403,6 +458,59 @@ func printPathColor(path string, cl *colorlib.ColorLib) error {
 // 返回值:
 // colorString: 经过颜色处理后的路径字符串。
 func getColorString(info globals.ListInfo, pF string, cl *colorlib.ColorLib) (colorString string) {
+	// 根据平台选择相应的颜色处理函数
+	if runtime.GOOS == "windows" {
+		colorString = windowsColor(info, pF, cl)
+	} else {
+		colorString = linuxColor(info, pF, cl)
+	}
+
+	return colorString
+}
+
+// linuxColor 函数的作用是在 Linux 系统下设置终端的颜色配置。
+func linuxColor(info globals.ListInfo, pF string, cl *colorlib.ColorLib) (colorString string) {
+	// 依据文件的类型来确定输出的颜色
+	switch info.EntryType {
+	case globals.DirType:
+		// 若文件类型为目录，则使用蓝色来渲染字符串
+		colorString = cl.Sblue(pF)
+	case globals.SymlinkType:
+		// 若文件类型为符号链接，则使用青色来渲染字符串
+		colorString = cl.Scyan(pF)
+	case globals.ExecutableType:
+		// 若文件类型为可执行文件，则使用绿色来渲染字符串
+		colorString = cl.Sgreen(pF)
+	case globals.SocketType, globals.PipeType, globals.BlockDeviceType, globals.CharDeviceType:
+		// 若文件类型为套接字、管道、块设备、字符设备，则使用黄色来渲染字符串
+		colorString = cl.Syellow(pF)
+	case globals.EmptyType:
+		// 若文件类型为空文件, 则使用灰色来渲染字符串
+		colorString = cl.Sgray(pF)
+	case globals.FileType:
+		// 若文件类型为普通文件，则根据文件后缀名来确定颜色
+		for color, extensions := range linuxColorMap {
+			if extensions[info.FileExt] {
+				switch color {
+				case "red":
+					colorString = cl.Sred(pF)
+				}
+				return colorString
+			}
+		}
+
+		// 若没有找到匹配的文件后缀名, 则使用白色来渲染字符串
+		colorString = cl.Swhite(pF)
+	default:
+		// 对于未匹配的类型，使用灰色来渲染字符串
+		colorString = cl.Sgray(pF)
+	}
+
+	return colorString
+}
+
+// windowsColor 函数的作用是在 Windows 系统下设置终端的颜色配置。
+func windowsColor(info globals.ListInfo, pF string, cl *colorlib.ColorLib) (colorString string) {
 	// 依据文件的类型来确定输出的颜色
 	switch info.EntryType {
 	case globals.DirType:
@@ -418,11 +526,11 @@ func getColorString(info globals.ListInfo, pF string, cl *colorlib.ColorLib) (co
 		// 若文件类型为套接字、管道、块设备、字符设备，则使用黄色来渲染字符串
 		colorString = cl.Syellow(pF)
 	case globals.EmptyType:
-		// 若文件类型为空文件, 则使用黑色来渲染字符串
-		colorString = cl.Sblack(pF)
+		// 若文件类型为空文件, 则使用灰色来渲染字符串
+		colorString = cl.Sgray(pF)
 	case globals.FileType:
 		// 若文件类型为普通文件，则根据文件后缀名来确定颜色
-		for color, extensions := range ColorMap {
+		for color, extensions := range windowsColorMap {
 			if extensions[info.FileExt] {
 				switch color {
 				case "yellow":
