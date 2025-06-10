f828bc4e6d6a0a9b93c438a69d66853547824d60
M乔木 <2776617348@qq.com>
2025-06-10 23:25:39 +0800

    refactor(cmd): 统一错误信息输出格式
    - 将所有的错误信息输出格式改为 "err: %v\n"，提高代码一致性
    - 简化错误信息的打印逻辑，便于维护和阅读

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -15,7 +15,7 @@ func Run() {
 	defer func() {
 		if err := recover(); err != nil {
 			// 打印错误信息并退出
-			fmt.Printf("fck在运行过程中发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 	}()
@@ -45,7 +45,7 @@ func Run() {
 	case "hash":
 		// 解析 hash 子命令的参数
 		if err := hashCmd.Parse(flag.Args()[1:]); err != nil {
-			fmt.Printf("解析hash子命令的参数时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 
@@ -57,13 +57,13 @@ func Run() {
 
 		// 执行 hash 子命令
 		if err := hashCmdMain(hashCmd, cmdCl); err != nil {
-			fmt.Printf("执行hash子命令时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 	case "h":
 		// 解析 hash 子命令的参数
 		if err := hashCmd.Parse(flag.Args()[1:]); err != nil {
-			fmt.Printf("解析hash子命令的参数时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 
@@ -75,13 +75,13 @@ func Run() {
 
 		// 执行 hash 子命令
 		if err := hashCmdMain(hashCmd, cmdCl); err != nil {
-			fmt.Printf("执行hash子命令时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 	case "size":
 		// 解析 size 子命令的参数
 		if err := sizeCmd.Parse(flag.Args()[1:]); err != nil {
-			fmt.Printf("解析size子命令的参数时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 
@@ -93,13 +93,13 @@ func Run() {
 
 		// 执行 size 子命令
 		if err := sizeCmdMain(sizeCmd, cmdCl); err != nil {
-			fmt.Printf("执行size子命令时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 	case "s":
 		// 解析 size 子命令的参数
 		if err := sizeCmd.Parse(flag.Args()[1:]); err != nil {
-			fmt.Printf("解析size子命令的参数时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 
@@ -111,13 +111,13 @@ func Run() {
 
 		// 执行 size 子命令
 		if err := sizeCmdMain(sizeCmd, cmdCl); err != nil {
-			fmt.Printf("执行size子命令时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 	case "diff":
 		// 解析 diff 子命令的参数
 		if err := diffCmd.Parse(flag.Args()[1:]); err != nil {
-			fmt.Printf("解析diff子命令的参数时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 
@@ -129,13 +129,13 @@ func Run() {
 
 		// 执行 diff 子命令
 		if err := diffCmdMain(cmdCl); err != nil {
-			fmt.Printf("执行diff子命令时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 	case "d":
 		// 解析 diff 子命令的参数
 		if err := diffCmd.Parse(flag.Args()[1:]); err != nil {
-			fmt.Printf("解析diff子命令的参数时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 
@@ -147,13 +147,13 @@ func Run() {
 
 		// 执行 diff 子命令
 		if err := diffCmdMain(cmdCl); err != nil {
-			fmt.Printf("执行diff子命令时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 	case "find":
 		// 解析 find 子命令的参数
 		if err := findCmd.Parse(flag.Args()[1:]); err != nil {
-			fmt.Printf("解析find子命令的参数时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 
@@ -165,13 +165,13 @@ func Run() {
 
 		// 执行 find 子命令
 		if err := findCmdMain(cmdCl, findCmd); err != nil {
-			fmt.Printf("执行find子命令时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 	case "f":
 		// 解析 find 子命令的参数
 		if err := findCmd.Parse(flag.Args()[1:]); err != nil {
-			fmt.Printf("解析find子命令的参数时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 
@@ -183,13 +183,13 @@ func Run() {
 
 		// 执行 find 子命令
 		if err := findCmdMain(cmdCl, findCmd); err != nil {
-			fmt.Printf("执行find子命令时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 	case "list":
 		// 解析 list 子命令的参数
 		if err := listCmd.Parse(flag.Args()[1:]); err != nil {
-			fmt.Printf("解析list子命令的参数时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 		// 如果是 -h 或 help, 则打印帮助信息并退出
@@ -199,13 +199,13 @@ func Run() {
 		}
 		// 执行 list 子命令
 		if err := listCmdMain(cmdCl, listCmd); err != nil {
-			fmt.Printf("执行list子命令时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 	case "ls":
 		// 解析 list 子命令的参数
 		if err := listCmd.Parse(flag.Args()[1:]); err != nil {
-			fmt.Printf("解析list子命令的参数时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 		// 如果是 -h 或 help, 则打印帮助信息并退出
@@ -215,7 +215,7 @@ func Run() {
 		}
 		// 执行 list 子命令
 		if err := listCmdMain(cmdCl, listCmd); err != nil {
-			fmt.Printf("执行list子命令时发生了错误: %v\n", err)
+			fmt.Printf("err: %v\n", err)
 			os.Exit(1)
 		}
 	default:
