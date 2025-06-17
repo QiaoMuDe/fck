a5bc720ae51097dd0d9a72f1f02eb702c4f651a8
M乔木 <2776617348@qq.com>
2025-06-17 21:27:32 +0800

    refactor(cmd): 重构列表命令的表格渲染逻辑
    - 提取了 prepareFileNames、renderDefaultTable 和 renderFileTable 函数，以简化代码结构
    - 优化了文件名处理和表格渲染的逻辑
    - 删除了重复代码，提高了代码的可读性和可维护性
    - 保留了原有的功能和命令行参数支持

--- cmd/cmd_list.go
+++ cmd/cmd_list.go
@@ -13,8 +13,6 @@ import (
 
 	"gitee.com/MM-Q/colorlib"
 	"gitee.com/MM-Q/fck/globals"
-	"github.com/jedib0t/go-pretty/v6/table"
-	"github.com/jedib0t/go-pretty/v6/text"
 	"golang.org/x/term"
 )
 
@@ -95,18 +93,27 @@ func listCmdMain(cl *colorlib.ColorLib, cmd *flag.FlagSet) error {
 		}
 
 		if pathInfo.IsDir() {
-			// 根据命令行参数排序目录中的文件信息
+			// 根据命令行参数排序目录中的文件信息 //
+			// 若同时指定按时间排序且不反转排序顺序，则按修改时间降序排序
 			if *listCmdSortByTime && !*listCmdReverseSort {
 				infos.SortByModTimeDesc()
+				// 若同时指定按时间排序且反转排序顺序，则按修改时间升序排序
 			} else if *listCmdSortByTime && *listCmdReverseSort {
 				infos.SortByModTimeAsc()
+				// 若同时指定按文件大小排序且不反转排序顺序，则按文件大小降序排序
 			} else if *listCmdSortBySize && !*listCmdReverseSort {
 				infos.SortByFileSizeDesc()
+				// 若同时指定按文件大小排序且反转排序顺序，则按文件大小升序排序
 			} else if *listCmdSortBySize && *listCmdReverseSort {
 				infos.SortByFileSizeAsc()
+				// 若同时指定按文件名排序且反转排序顺序，则按文件名升序排序
 			} else if *listCmdSortByName && *listCmdReverseSort {
 				infos.SortByFileNameAsc()
+				// 默认为按文件名降序排序, 如果仅指定反转排序顺序，则按文件名升序排序
+			} else if *listCmdReverseSort {
+				fileInfos.SortByFileNameAsc()
 			} else {
+				// 其他情况，按文件名降序排序
 				infos.SortByFileNameDesc()
 			}
 
@@ -150,18 +157,27 @@ func listCmdMain(cl *colorlib.ColorLib, cmd *flag.FlagSet) error {
 		// 打印文件组标题
 		cl.Green("Files:")
 
-		// 根据命令行参数排序文件信息切片
+		// 根据命令行参数排序文件信息切片 //
+		// 若同时指定按时间排序且不反转排序顺序，则按修改时间降序排序
 		if *listCmdSortByTime && !*listCmdReverseSort {
 			fileInfos.SortByModTimeDesc()
+			// 若同时指定按时间排序且反转排序顺序，则按修改时间升序排序
 		} else if *listCmdSortByTime && *listCmdReverseSort {
 			fileInfos.SortByModTimeAsc()
+			// 若同时指定按文件大小排序且不反转排序顺序，则按文件大小降序排序
 		} else if *listCmdSortBySize && !*listCmdReverseSort {
 			fileInfos.SortByFileSizeDesc()
+			// 若同时指定按文件大小排序且反转排序顺序，则按文件大小升序排序
 		} else if *listCmdSortBySize && *listCmdReverseSort {
 			fileInfos.SortByFileSizeAsc()
+			// 若同时指定按文件名排序且反转排序顺序，则按文件名升序排序
 		} else if *listCmdSortByName && *listCmdReverseSort {
 			fileInfos.SortByFileNameAsc()
+			// 默认为按文件名降序排序, 如果仅指定反转排序顺序，则按文件名升序排序
+		} else if *listCmdReverseSort {
+			fileInfos.SortByFileNameAsc()
 		} else {
+			// 其他情况，按文件名降序排序
 			fileInfos.SortByFileNameDesc()
 		}
 
@@ -252,67 +268,15 @@ func listCmdDefault(cl *colorlib.ColorLib, lfs globals.ListInfos) error {
 		// 处理每个目录
 		for _, dir := range dirs {
 			infos := dirFiles[dir]
-			fileNames := make([]string, len(infos))
-
-			// 准备文件名
-			for i, info := range infos {
-				_, file := filepath.Split(info.Name)
-				var paddedFilename string
-				if *listCmdQuoteNames {
-					paddedFilename = fmt.Sprintf("%q", file)
-				} else {
-					paddedFilename = file
-				}
-
-				// 检查是否启用颜色输出
-				if *listCmdColor {
-					paddedFilename = getColorString(info, paddedFilename, cl)
-				}
-
-				fileNames[i] = paddedFilename
-			}
-
-			// 动态计算每行可以容纳的列数
-			maxWidth := text.LongestLineLen(strings.Join(fileNames, "\n"))
-			columns := width / (maxWidth + 2) // 每列增加2个字符的间距
-			if columns == 0 {
-				columns = 1 // 至少显示一列
-			}
+			fileNames := prepareFileNames(infos, *listCmdQuoteNames, *listCmdColor, cl)
 
 			// 打印目录名
 			cl.Bluef("%s:\n", dir)
 
-			// 创建表格
-			t := table.NewWriter()
-			t.SetOutputMirror(os.Stdout)
-
-			// 构建多列输出
-			for i := 0; i < len(fileNames); i += columns {
-				end := i + columns
-				if end > len(fileNames) {
-					end = len(fileNames)
-				}
-
-				// 创建一个临时表格行
-				row := make([]interface{}, end-i)
-				for j := i; j < end; j++ {
-					row[j-i] = fileNames[j]
-				}
-
-				// 添加行到表格
-				t.AppendRow(row)
+			// 创建并渲染表格
+			if err := renderDefaultTable(fileNames, width, *listCmdTableStyle); err != nil {
+				return fmt.Errorf("渲染目录 %s 表格失败: %v", dir, err)
 			}
-
-			// 设置表格样式
-			if *listCmdTableStyle != "" {
-				// 根据-ts的值设置表格样式
-				if style, ok := globals.TableStyleMap[*listCmdTableStyle]; ok {
-					t.SetStyle(style)
-				}
-			}
-
-			// 输出表格
-			t.Render()
 			fmt.Println() // 目录间空行
 		}
 
@@ -320,63 +284,10 @@ func listCmdDefault(cl *colorlib.ColorLib, lfs globals.ListInfos) error {
 	}
 
 	// 非递归模式，直接输出所有文件
-	fileNames := make([]string, len(lfs))
-	for i, info := range lfs {
-		var paddedFilename string
-		if *listCmdQuoteNames {
-			paddedFilename = fmt.Sprintf("%q", info.Name)
-		} else {
-			paddedFilename = info.Name
-		}
+	fileNames := prepareFileNames(lfs, *listCmdQuoteNames, *listCmdColor, cl)
 
-		// 检查是否启用颜色输出
-		if *listCmdColor {
-			paddedFilename = getColorString(info, paddedFilename, cl)
-		}
-
-		fileNames[i] = paddedFilename
-	}
-
-	// 动态计算每行可以容纳的列数
-	maxWidth := text.LongestLineLen(strings.Join(fileNames, "\n"))
-	columns := width / (maxWidth + 2) // 每列增加2个字符的间距
-	if columns == 0 {
-		columns = 1 // 至少显示一列
-	}
-
-	// 创建表格
-	t := table.NewWriter()
-	t.SetOutputMirror(os.Stdout)
-
-	// 构建多列输出
-	for i := 0; i < len(fileNames); i += columns {
-		end := i + columns
-		if end > len(fileNames) {
-			end = len(fileNames)
-		}
-
-		// 创建一个临时表格行
-		row := make([]interface{}, end-i)
-		for j := i; j < end; j++ {
-			row[j-i] = fileNames[j]
-		}
-
-		// 添加行到表格
-		t.AppendRow(row)
-	}
-
-	// 设置表格样式
-	if *listCmdTableStyle != "" {
-		// 根据-ts的值设置表格样式
-		if style, ok := globals.TableStyleMap[*listCmdTableStyle]; ok {
-			t.SetStyle(style)
-		}
-	}
-
-	// 输出表格
-	t.Render()
-
-	return nil
+	// 创建并渲染表格
+	return renderDefaultTable(fileNames, width, *listCmdTableStyle)
 }
 
 // listCmdLong 函数用于以长格式输出文件信息，支持递归目录分组显示。
@@ -407,184 +318,17 @@ func listCmdLong(cl *colorlib.ColorLib, ifs globals.ListInfos) error {
 			// 打印目录名
 			cl.Bluef("%s:\n", dir)
 
-			// 创建表格
-			t := table.NewWriter()
-			t.SetOutputMirror(os.Stdout)
-
-			// 设置表头, 只有在-ts为none时才设置表头
-			if *listCmdTableStyle != "none" {
-				if *listCmdShowUserGroup {
-					t.AppendHeader(table.Row{"Type", "Perm", "Owner", "Group", "Size", "Unit", "ModTime", "Name"})
-				} else {
-					t.AppendHeader(table.Row{"Type", "Perm", "Size", "Unit", "ModTime", "Name"})
-				}
+			// 创建并渲染表格
+			if err := renderFileTable(cl, infos); err != nil {
+				return fmt.Errorf("渲染目录 %s 表格失败: %v", dir, err)
 			}
-
-			// 添加文件信息行
-			for _, info := range infos {
-				// 获取文件名（仅文件名，不含路径）
-				_, fileName := filepath.Split(info.Name)
-
-				// 获取文件权限字符串
-				infoPerm := FormatPermissionString(cl, info)
-
-				// 类型
-				infoType := getColorString(info, info.EntryType, cl)
-
-				// 文件大小 和 单位
-				infoSize, infoSizeUnit := humanSize(info.Size)
-
-				// 渲染文件大小
-				infoSize = cl.Syellow(infoSize)
-
-				// 渲染文件大小单位
-				infoSizeUnit = cl.Syellow(infoSizeUnit)
-
-				// 修改时间 禁用加粗
-				cl.NoBold.Store(true)
-				infoModTime := cl.Sblue(info.ModTime.Format("2006-01-02 15:04:05"))
-				cl.NoBold.Store(false)
-
-				// 文件名
-				var infoName string
-				// 检查是否启用引号
-				if *listCmdQuoteNames {
-					// 检查是否为软链接
-					if info.EntryType == globals.SymlinkType {
-						infoName = getColorString(info, fmt.Sprintf("%q -> %q", fileName, info.LinkTargetPath), cl)
-					} else {
-						infoName = getColorString(info, fmt.Sprintf("%q", fileName), cl)
-					}
-				} else {
-					// 检查是否为软链接
-					if info.EntryType == globals.SymlinkType {
-						infoName = getColorString(info, fmt.Sprintf("%s -> %s", fileName, info.LinkTargetPath), cl)
-					} else {
-						infoName = getColorString(info, fileName, cl)
-					}
-				}
-
-				// 添加行到表格
-				if *listCmdShowUserGroup {
-					t.AppendRow(table.Row{infoType, infoPerm, info.Owner, info.Group, infoSize, infoSizeUnit, infoModTime, infoName})
-				} else {
-					t.AppendRow(table.Row{infoType, infoPerm, infoSize, infoSizeUnit, infoModTime, infoName})
-				}
-			}
-
-			// 设置列的对齐方式
-			t.SetColumnConfigs([]table.ColumnConfig{
-				{Name: "Size", Align: text.AlignRight},     // 文件大小 - 右对齐
-				{Name: "Type", Align: text.AlignCenter},    // 文件类型 - 居中对齐
-				{Name: "Owner", Align: text.AlignCenter},   // 所有者 - 居中对齐
-				{Name: "Group", Align: text.AlignCenter},   // 组 - 居中对齐
-				{Name: "Perm", Align: text.AlignLeft},      // 权限 - 左对齐
-				{Name: "ModTime", Align: text.AlignCenter}, // 修改时间 - 居中对齐
-				{Name: "Unit", Align: text.AlignCenter},    // 单位 - 居中对齐
-				{Name: "Name", Align: text.AlignLeft},      // 文件名 - 左对齐
-			})
-
-			// 设置表格样式
-			if *listCmdTableStyle != "" {
-				// 根据-ts的值设置表格样式
-				if style, ok := globals.TableStyleMap[*listCmdTableStyle]; ok {
-					t.SetStyle(style)
-				}
-			}
-
-			// 输出表格
-			t.Render()
 			fmt.Println() // 目录间空行
 		}
 		return nil
 	}
 
-	// 非递归模式，使用原始单个表格输出
-	// 创建表格
-	t := table.NewWriter()
-	t.SetOutputMirror(os.Stdout)
-
-	// 设置表头, 只有在-ts为none时才设置表头
-	if *listCmdTableStyle != "none" {
-		if *listCmdShowUserGroup {
-			t.AppendHeader(table.Row{"Type", "Perm", "Owner", "Group", "Size", "Unit", "ModTime", "Name"})
-		} else {
-			t.AppendHeader(table.Row{"Type", "Perm", "Size", "Unit", "ModTime", "Name"})
-		}
-	}
-
-	for _, info := range ifs {
-		// 获取文件权限字符串
-		infoPerm := FormatPermissionString(cl, info)
-
-		// 类型
-		infoType := getColorString(info, info.EntryType, cl)
-
-		// 文件大小 和 单位
-		infoSize, infoSizeUnit := humanSize(info.Size)
-
-		// 渲染文件大小
-		infoSize = cl.Syellow(infoSize)
-
-		// 渲染文件大小单位
-		infoSizeUnit = cl.Syellow(infoSizeUnit)
-
-		// 修改时间 禁用加粗
-		cl.NoBold.Store(true)
-		infoModTime := cl.Sblue(info.ModTime.Format("2006-01-02 15:04:05"))
-		cl.NoBold.Store(false)
-
-		// 文件名
-		var infoName string
-		// 检查是否启用引号
-		if *listCmdQuoteNames {
-			// 检查是否为软链接
-			if info.EntryType == globals.SymlinkType {
-				infoName = getColorString(info, fmt.Sprintf("%q -> %q", info.Name, info.LinkTargetPath), cl)
-			} else {
-				infoName = getColorString(info, fmt.Sprintf("%q", info.Name), cl)
-			}
-		} else {
-			// 检查是否为软链接
-			if info.EntryType == globals.SymlinkType {
-				infoName = getColorString(info, fmt.Sprintf("%s -> %s", info.Name, info.LinkTargetPath), cl)
-			} else {
-				infoName = getColorString(info, info.Name, cl)
-			}
-		}
-
-		// 添加行到表格
-		if *listCmdShowUserGroup {
-			t.AppendRow(table.Row{infoType, infoPerm, info.Owner, info.Group, infoSize, infoSizeUnit, infoModTime, infoName})
-		} else {
-			t.AppendRow(table.Row{infoType, infoPerm, infoSize, infoSizeUnit, infoModTime, infoName})
-		}
-	}
-
-	// 设置列的对齐方式
-	t.SetColumnConfigs([]table.ColumnConfig{
-		{Name: "Size", Align: text.AlignRight},     // 文件大小 - 右对齐
-		{Name: "Type", Align: text.AlignCenter},    // 文件类型 - 居中对齐
-		{Name: "Owner", Align: text.AlignCenter},   // 所有者 - 居中对齐
-		{Name: "Group", Align: text.AlignCenter},   // 组 - 居中对齐
-		{Name: "Perm", Align: text.AlignLeft},      // 权限 - 左对齐
-		{Name: "ModTime", Align: text.AlignCenter}, // 修改时间 - 居中对齐
-		{Name: "Unit", Align: text.AlignCenter},    // 单位 - 居中对齐
-		{Name: "Name", Align: text.AlignLeft},      // 文件名 - 左对齐
-	})
-
-	// 设置表格样式
-	if *listCmdTableStyle != "" {
-		// 根据-ts的值设置表格样式
-		if style, ok := globals.TableStyleMap[*listCmdTableStyle]; ok {
-			t.SetStyle(style)
-		}
-	}
-
-	// 输出表格
-	t.Render()
-
-	return nil
+	// 非递归模式，直接渲染表格
+	return renderFileTable(cl, ifs)
 }
 
 // FormatPermissionString 根据颜色模式格式化权限字符串
---
+++ cmd/cmd_table.go
@@ -0,0 +1,204 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"gitee.com/MM-Q/colorlib"
+	"gitee.com/MM-Q/fck/globals"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
+)
+
+// prepareFileNames 准备文件名列表，处理引号和颜色
+// 参数:
+//
+//	infos - 文件信息列表
+//	quoteNames - 是否对文件名添加引号
+//	useColor - 是否启用颜色输出
+//	cl - 颜色库实例
+//
+// 返回:
+//
+//	格式化后的文件名列表
+func prepareFileNames(infos globals.ListInfos, quoteNames bool, useColor bool, cl *colorlib.ColorLib) []string {
+	fileNames := make([]string, len(infos))
+	for i, info := range infos {
+		_, file := filepath.Split(info.Name)
+		var paddedFilename string
+		if quoteNames {
+			paddedFilename = fmt.Sprintf("%q", file)
+		} else {
+			paddedFilename = file
+		}
+
+		// 检查是否启用颜色输出
+		if useColor {
+			paddedFilename = getColorString(info, paddedFilename, cl)
+		}
+
+		fileNames[i] = paddedFilename
+	}
+	return fileNames
+}
+
+// renderDefaultTable 创建并渲染默认格式的多列表格
+// 参数:
+//
+//	fileNames - 文件名列表
+//	terminalWidth - 终端宽度
+//	tableStyle - 表格样式名称
+//
+// 返回:
+//
+//	错误信息，如果有
+func renderDefaultTable(fileNames []string, terminalWidth int, tableStyle string) error {
+	// 动态计算每行可以容纳的列数
+	maxWidth := text.LongestLineLen(strings.Join(fileNames, "\n"))
+
+	// 每列增加2个字符的间距
+	columns := terminalWidth / (maxWidth + 2)
+	if columns == 0 {
+		columns = 1 // 至少显示一列
+	}
+
+	// 创建表格
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+
+	// 构建多列输出
+	for i := 0; i < len(fileNames); i += columns {
+		// 计算当前行的结束位置
+		end := i + columns
+
+		// 确保结束位置不超过文件名列表长度
+		if end > len(fileNames) {
+			end = len(fileNames)
+		}
+
+		// 创建一个临时表格行
+		row := make([]interface{}, end-i)
+		for j := i; j < end; j++ {
+			row[j-i] = fileNames[j]
+		}
+
+		// 添加行到表格
+		t.AppendRow(row)
+	}
+
+	// 设置表格样式
+	if tableStyle != "" {
+		// 根据表格样式名称设置样式
+		if style, ok := globals.TableStyleMap[tableStyle]; ok {
+			t.SetStyle(style)
+		}
+	}
+
+	// 输出表格
+	t.Render()
+
+	return nil
+}
+
+// renderFileTable 创建并渲染文件信息表格
+// 参数:
+//
+//	cl - 颜色库实例
+//	infos - 要显示的文件信息列表
+//
+// 返回:
+//
+//	错误信息，如果有
+func renderFileTable(cl *colorlib.ColorLib, infos globals.ListInfos) error {
+	// 创建表格
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+
+	// 设置表头
+	if *listCmdTableStyle != "none" {
+		if *listCmdShowUserGroup {
+			t.AppendHeader(table.Row{"Type", "Perm", "Owner", "Group", "Size", "Unit", "ModTime", "Name"})
+		} else {
+			t.AppendHeader(table.Row{"Type", "Perm", "Size", "Unit", "ModTime", "Name"})
+		}
+	}
+
+	// 添加文件信息行
+	for _, info := range infos {
+		// 获取文件名（仅文件名，不含路径）
+		_, fileName := filepath.Split(info.Name)
+
+		// 获取文件权限字符串
+		infoPerm := FormatPermissionString(cl, info)
+
+		// 类型
+		infoType := getColorString(info, info.EntryType, cl)
+
+		// 文件大小 和 单位
+		infoSize, infoSizeUnit := humanSize(info.Size)
+
+		// 渲染文件大小
+		cl.NoBold.Store(true)
+		infoSize = cl.Syellow(infoSize)
+
+		// 渲染文件大小单位
+		infoSizeUnit = cl.Syellow(infoSizeUnit)
+
+		// 修改时间 禁用加粗
+		infoModTime := cl.Sblue(info.ModTime.Format("2006-01-02 15:04:05"))
+		cl.NoBold.Store(false)
+
+		// 文件名
+		var infoName string
+		// 检查是否启用引号
+		if *listCmdQuoteNames {
+			// 检查是否为软链接
+			if info.EntryType == globals.SymlinkType {
+				infoName = getColorString(info, fmt.Sprintf("%q -> %q", fileName, info.LinkTargetPath), cl)
+			} else {
+				infoName = getColorString(info, fmt.Sprintf("%q", fileName), cl)
+			}
+		} else {
+			// 检查是否为软链接
+			if info.EntryType == globals.SymlinkType {
+				infoName = getColorString(info, fmt.Sprintf("%s -> %s", fileName, info.LinkTargetPath), cl)
+			} else {
+				infoName = getColorString(info, fileName, cl)
+			}
+		}
+
+		// 添加行到表格
+		if *listCmdShowUserGroup {
+			t.AppendRow(table.Row{infoType, infoPerm, info.Owner, info.Group, infoSize, infoSizeUnit, infoModTime, infoName})
+		} else {
+			t.AppendRow(table.Row{infoType, infoPerm, infoSize, infoSizeUnit, infoModTime, infoName})
+		}
+	}
+
+	// 设置列的对齐方式
+	t.SetColumnConfigs([]table.ColumnConfig{
+		{Name: "Size", Align: text.AlignRight},     // 文件大小 - 右对齐
+		{Name: "Type", Align: text.AlignCenter},    // 文件类型 - 居中对齐
+		{Name: "Owner", Align: text.AlignCenter},   // 所有者 - 居中对齐
+		{Name: "Group", Align: text.AlignCenter},   // 组 - 居中对齐
+		{Name: "Perm", Align: text.AlignLeft},      // 权限 - 左对齐
+		{Name: "ModTime", Align: text.AlignCenter}, // 修改时间 - 居中对齐
+		{Name: "Unit", Align: text.AlignCenter},    // 单位 - 居中对齐
+		{Name: "Name", Align: text.AlignLeft},      // 文件名 - 左对齐
+	})
+
+	// 设置表格样式
+	if *listCmdTableStyle != "" {
+		// 根据-ts的值设置表格样式
+		if style, ok := globals.TableStyleMap[*listCmdTableStyle]; ok {
+			t.SetStyle(style)
+		}
+	}
+
+	// 输出表格
+	t.Render()
+
+	return nil
+}
