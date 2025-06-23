6031f8192fe8a5a4feaaca8252bd6ec31453218d
M乔木 <2776617348@qq.com>
2025-06-23 17:24:13 +0800

    refactor(cmd): 注释掉文件组标题的打印
    - 在 listCmdMain 函数中注释掉了打印文件组标题 "Files:" 的代码行
    - 这个修改可能是为了简化输出或准备替换为更灵活的显示方式

--- cmd/cmd_list.go
+++ cmd/cmd_list.go
@@ -145,7 +145,7 @@ func listCmdMain(cl *colorlib.ColorLib) error {
 	// 处理所有文件
 	if len(fileInfos) > 0 {
 		// 打印文件组标题
-		cl.Green("Files:")
+		//cl.Green("Files:")
 
 		// 根据命令行参数排序文件信息切片
 		sortFileInfos(fileInfos, listCmdSortByTime.Get(), listCmdSortBySize.Get(), listCmdSortByName.Get(), listCmdReverseSort.Get())
