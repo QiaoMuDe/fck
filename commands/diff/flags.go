95680275c182955c01de44676be264541e011791
M乔木 <2776617348@qq.com>
2025-08-17 10:04:24 +0800

    refactor(diff): 重构 diff 命令功能
    - 移除了目录对比功能，只保留文件校验功能
    - 删除了相关的代码和导入
    - 优化了代码结构，提高了代码可读性和维护性

--- commands/diff/flags.go
+++ commands/diff/flags.go
@@ -10,13 +10,8 @@ import (
 
 var (
 	// fck diff 子命令
-	diffCmd      *cmd.Cmd
-	diffCmdFile  *qflag.StringFlag // file 标志
-	diffCmdDirs  *qflag.StringFlag // dirs 标志
-	diffCmdDirA  *qflag.StringFlag // dirA 标志
-	diffCmdDirB  *qflag.StringFlag // dirB 标志
-	diffCmdType  *qflag.EnumFlag   // type 标志
-	diffCmdWrite *qflag.BoolFlag   // write 标志
+	diffCmd     *cmd.Cmd
+	diffCmdFile *qflag.StringFlag // file 标志
 )
 
 func InitDiffCmd() *cmd.Cmd {
@@ -27,11 +22,6 @@ func InitDiffCmd() *cmd.Cmd {
 		WithNote("校验文件必须包含有效的头信息").
 		WithNote("校验时会自动跳过空行和注释行(以#开头的行)")
 	diffCmdFile = diffCmd.String("file", "f", types.OutputFileName, "指定用于校验的哈希值文件，程序将依据该文件中的哈希值进行校验操作")
-	diffCmdDirs = diffCmd.String("dir", "d", "", "指定需要根据哈希值文件进行校验的目标目录")
-	diffCmdDirA = diffCmd.String("dirA", "a", "", "指定要对比的目录A")
-	diffCmdDirB = diffCmd.String("dirB", "b", "", "指定要对比的目录B")
-	diffCmdWrite = diffCmd.Bool("write", "w", false, "将校验结果写入文件, 文件名为check_dir.check")
-	diffCmdType = diffCmd.Enum("type", "t", "md5", "指定哈希算法，支持 md5、sha1、sha256、sha512", []string{"md5", "sha1", "sha256", "sha512"})
 
 	// 创建并返回一个命令对象
 	return diffCmd
