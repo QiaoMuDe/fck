f4c266a5a81a65bc46e100c182bf743c3ceeeb0d
M乔木 <2776617348@qq.com>
2025-09-23 14:32:54 +0800

    refactor(flags): 优化主命令初始化逻辑并更新依赖版本
    - 移除未使用的 "runtime" 包导入
    - 简化 InItMainCmd 函数实现，调整 qflag 版本设置方式
    - 更新 verman 和 shellx 依赖至最新版本
    - 调整 shellx 内部命令字符串生成逻辑，提升代码可读性
    - 同步更新 verman 库的文档与示例，完善 README 内容

--- commands/flags.go
+++ commands/flags.go
@@ -3,21 +3,18 @@
 package commands
 
 import (
-	"runtime"
-
 	"gitee.com/MM-Q/fck/commands/internal/types"
 	"gitee.com/MM-Q/qflag"
 	"gitee.com/MM-Q/verman"
 )
 
 // InitMainCmd 初始化主命令
-func InItMainCmd() {
-	v := verman.Get()                                                                               // 获取版本信息
-	qflag.SetVersionf("%s version %s %s/%s", v.AppName, v.GitVersion, runtime.GOOS, runtime.GOARCH) // 设置版本信息
-	qflag.SetUseChinese(true)                                                                       // 启用中文帮助信息
-	qflag.SetDescription("多功能文件处理工具集, 提供文件哈希计算、大小统计、查找和校验等实用功能")                                    // 设置命令行描述
-	qflag.AddNote("各子命令有独立帮助文档，可通过-h参数查看, 例如 'fck <子命令> -h' 查看各子命令详细帮助")                            // 设置命令行note
-	qflag.AddNote("所有路径参数支持Windows和Unix风格")                                                         // 添加命令行note
-	qflag.SetLogoText(types.FckHelpLogo)                                                            // 设置命令行logo
-	qflag.SetEnableCompletion(true)                                                                 // 启用自动补全
+func InItMainCmd() { // 获取版本信息
+	qflag.SetVersion(verman.V.Version())                                 // 设置版本信息
+	qflag.SetUseChinese(true)                                            // 启用中文帮助信息
+	qflag.SetDescription("多功能文件处理工具集, 提供文件哈希计算、大小统计、查找和校验等实用功能")         // 设置命令行描述
+	qflag.AddNote("各子命令有独立帮助文档，可通过-h参数查看, 例如 'fck <子命令> -h' 查看各子命令详细帮助") // 设置命令行note
+	qflag.AddNote("所有路径参数支持Windows和Unix风格")                              // 添加命令行note
+	qflag.SetLogoText(types.FckHelpLogo)                                 // 设置命令行logo
+	qflag.SetEnableCompletion(true)                                      // 启用自动补全
 }
